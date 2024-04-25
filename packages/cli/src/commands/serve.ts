d7933212af7468ce6a6d3e66a39af1e22c613b9f
JeonghakHeo <jeonghak4142@gmail.com>
2024-04-25 20:35:40 +1000

    setup proxy for dev and linking local-api with local-client

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -5,6 +5,7 @@ import { serve } from 'local-api'
 interface LocalApiError {
   code: string
 }
+const isProduction = process.env.NODE_ENV === 'production'
 
 export const serveCommand = new Command()
   .command('serve [filename]')
@@ -17,7 +18,12 @@ export const serveCommand = new Command()
 
     try {
       const dir = path.join(process.cwd(), path.dirname(filename))
-      await serve(parseInt(options.port), path.basename(filename), dir)
+      await serve(
+        parseInt(options.port),
+        path.basename(filename),
+        dir,
+        !isProduction
+      )
 
       console.log(
         `Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`
