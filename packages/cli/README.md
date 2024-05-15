67fa75d788ce04924751f662cb9e09b41ea09645
JeonghakHeo <jeonghak4142@gmail.com>
2024-05-15 11:17:31 +1000

    add snippets on initial render and fix typos in README

--- packages/cli/README.md
+++ packages/cli/README.md
@@ -2,9 +2,11 @@
 
 This is an interactive coding environment. You can write Javascript, see it executed, and write comprehensive documentation using markdown.
 
+![preview](public/preview.png)
+
 - Click any text cell to edit it
 - The code in each code editor is all joined together into one file. If you define a variable in cell #1, you can refer to it in any following cell!
-- You can show any React component, string, number, or anything else by calling the `show` function. This is a function built into this environment. Call show multiple times to `show` multiple values
+- You can show any React component, string, number, or anything else by calling the `show` function. This is a function built into this environment. Call show multiple times to show multiple values
 - Re-order or delete cells using the buttons on the top right
 - Add new cells by hovering on the divider between each cell
 
@@ -25,19 +27,16 @@ npx pocket-studio serve
 ## Commands
 
 ```
-serve [options] [filename] Open a file for editing
-help [command] display help for command
+serve [<filename>] [--port <port>]
 ```
 
 ## Options
 
 ```
--p -port Port to connect
+-p --port Port to the run local API server on
 
-serve -p 80
-serve -p80
-serve --port 80
-serve --port=80
+serve -p 4000
+serve --port 4000
 ```
 
 ## Usage
--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -37,7 +37,26 @@ export const createCellsRouter = (filename: string, dir: string) => {
       if (isLocalApiError(err)) {
         if (err.code === 'ENOENT') {
           await fs.writeFile(fullPath, '[]', 'utf-8')
-          res.send([])
+          res.send([
+            {
+              type: 'text',
+              content:
+                '## PocketStudio\n\nThis is an interactive coding environment. You can write Javascript, see it executed, and write comprehensive documentation using markdown.\n\n- Click any text cell to edit it\n- The code in each code editor is all joined together into one file. If you define a variable in cell #1, you can refer to it in any following cell!\n- You can show any React component, string, number, or anything else by calling the `show` function. This is a function built into this environment. Call show multiple times to show multiple values\n- Re-order or delete cells using the buttons on the top right\n- Add enw cells by hovering on the divider between each cell\n\nAll of your changes get saved to the file you opened PocketStudio with. So if you ran `npx pocket-studio serve test.js`, all of the text code you write will be saved to the `test.js` file.',
+              id: 'y06nv',
+            },
+            {
+              type: 'code',
+              content:
+                "import { useState } from 'react';\n\nconst Counter = () => {\n  const [count, setCount] = useState(0);\n  return (\n    <div>\n      <button onClick={() => setCount(count + 1)}>\n        Click\n      </button>\n      <h3>Count: {count}</h3>\n    </div>\n  );\n};\n\n// Display any variable or React component by calling 'show'\nshow(<Counter />);",
+              id: 'yp883',
+            },
+            {
+              type: 'code',
+              content:
+                'const App = () => {\n  return (\n    <div>\n      <h3>App Says Hi!</h3>\n      <i>Counter component will be rendered below...</i>\n      <hr />\n      {/*\n        Counter was declared in an earlier cell -\n        we can reference it here!\n       */}\n       <Counter />\n    </div>\n  );\n};\n\nshow(<App />);',
+              id: '3vfy5',
+            },
+          ])
         }
       } else {
         throw err
